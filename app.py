90769a5c0095dbafbd0eaa72ba13e23a88306ed3
TPLulu <thibault.piotrowski@gmail.com>
2024-03-21 12:13:07 +0100

    changement code

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     ip = request.host.split(':')[0]  # Obtenir l'adresse IP du serveur
-    return f"<p>Hello, World! This server is running on IP: {ip}</p>"
+    ip_address = request.remote_addr
+    return f"<p>Hello, World! This server is running on IP: {ip} and maybe : {ip_address} </p>"
 
 if __name__ == "__main__":
     app.run('0.0.0.0', port=5000)
