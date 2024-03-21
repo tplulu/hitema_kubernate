2cca05fe5cb188256c4e70cf068df8b2192d4bca
TPLulu <thibault.piotrowski@gmail.com>
2024-03-21 10:09:24 +0100

    modif githube action

--- app.py
+++ app.py
@@ -1,11 +1,2 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-   return "<p>Hello, World!</p>"
-
-if __name__== "__main__":
-
-   app.run('0.0.0.0',port=5000)
+a = 2
+print("coucou", a)
