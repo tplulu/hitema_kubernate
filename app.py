9b4b13dbc8434ae4c0fa9a6d848bd0213761aa0b
TPLulu <thibault.piotrowski@gmail.com>
2024-03-21 11:45:14 +0100

    ajout pour le réseau

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-   return "<p>Hello, World!</p>"
+    ip = request.host.split(':')[0]  # Obtenir l'adresse IP du serveur
+    return f"<p>Hello, World! This server is running on IP: {ip}</p>"
 
-if __name__== "__main__":
-
-   app.run('0.0.0.0',port=5000)
+if __name__ == "__main__":
+    app.run('0.0.0.0', port=5000)
