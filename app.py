b5d2e90f742a8128179323d8c948e84a41bbbc06
TPLulu <thibault.piotrowski@gmail.com>
2024-03-21 10:21:54 +0100

    pipeline build/piush image

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-a = 2
-print("coucou", a)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+   return "<p>Hello, World!</p>"
+
+if __name__== "__main__":
+
+   app.run('0.0.0.0',port=5000)
